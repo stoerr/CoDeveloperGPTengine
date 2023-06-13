fb44ec31f1ffb46b7be8ffd383482b7480e09daf
Hans-Peter Stoerr <hans-peter+git-noreply@stoerr.net>
2023-06-13 20:35:35 +0000

    ChatGPT: Create a pom.xml for a project running a Undertow http server, using Java version 16 in the corresponding maven.compiler.* properties. groupid should be net.stoerr.chatgpt, artifactid developersChatGPTtoolbenchPlugin.
    Create a java file net.stoerr.chatgpt.devtoolbench.DevToolbench that starts a http server and returns "Hello World!" to every get request on port 8080.
    Configure that as the main class for the maven generated jar.
    Use the shade plugin to create a jar that has all dependencies included.

--- src/main/java/net/stoerr/chatgpt/devtoolbench/DevToolbench.java
+++ src/main/java/net/stoerr/chatgpt/devtoolbench/DevToolbench.java
@@ -1,11 +1,22 @@
 package net.stoerr.chatgpt.devtoolbench;
 
-import static spark.Spark.*;
+import io.undertow.Undertow;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.util.Headers;
 
 public class DevToolbench {
 
-    public static void main(String[] args) {
-        get("/", (req, res) -> "Hello World!");
+    public static void main(final String[] args) {
+        Undertow server = Undertow.builder()
+                .addHttpListener(8080, "localhost")
+                .setHandler(new HttpHandler() {
+                    @Override
+                    public void handleRequest(final HttpServerExchange exchange) throws Exception {
+                        exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
+                        exchange.getResponseSender().send("Hello World!");
+                    }
+                }).build();
+        server.start();
     }
-
 }
