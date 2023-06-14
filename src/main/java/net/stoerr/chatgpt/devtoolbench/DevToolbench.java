943a3678f8ed1c842e1280ae487e221899b21798
Hans-Peter Stoerr <hans-peter+git-noreply@stoerr.net>
2023-06-14 20:17:58 +0000

    ChatGPT: Please compare that with the original code in net.stoerr.chatgpt.forChatGPTtoMigrate.FileManagerPlugin - there are special cases for /.well-known/ai-plugin.json and /filemanager.yaml (to be renamed to devtoolbench.yaml) that need to be transferred to the new code. ai-plugin.json should be saved in a file in src/main/resources and read when served through Undertow , but the other one needs to be constructed (plus several times feedback compilation and other errors.)

--- src/main/java/net/stoerr/chatgpt/devtoolbench/DevToolbench.java
+++ src/main/java/net/stoerr/chatgpt/devtoolbench/DevToolbench.java
@@ -5,44 +5,67 @@ import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.util.Headers;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Supplier;
 
 public class DevToolbench {
 
-    private static final Map<String, HttpHandler> handlers = new HashMap<>();
+    private static final Map<String, Supplier<String>> STATICFILES = new HashMap<>();
+    private static final Map<String, HttpHandler> HANDLERS = new HashMap<>();
 
-    public static void main(final String[] args) {
+    static {
+        HANDLERS.put("/listFiles", new ListFilesOperation());
+        HANDLERS.put("/readFile", new ReadFileOperation());
+        HANDLERS.put("/writeFile", new WriteFileOperation());
+        STATICFILES.put("/.well-known/ai-plugin.json", () -> {
+            try {
+                InputStream in = DevToolbench.class.getResourceAsStream("/ai-plugin.json");
+                if (in == null) {
+                    throw new RuntimeException("Could not find ai-plugin.json");
+                }
+                return new String(in.readAllBytes(), StandardCharsets.UTF_8);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        STATICFILES.put("/devtoolbench.yaml", () -> "..." /* TODO: Construct the YAML content here */);
+    }
+
+    public static void main(String[] args) {
         int port = args.length > 0 ? Integer.parseInt(args[0]) : 3001;
         Undertow server = Undertow.builder()
                 .addHttpListener(port, "localhost")
-                .setHandler(exchange -> {
-                    String path = exchange.getRequestPath();
-                    if (handlers.containsKey(path)) {
-                        try {
-                            handlers.get(path).handleRequest(exchange);
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                            exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
-                            exchange.getResponseSender().send("Internal server error: " + e.getMessage());
-                        }
-                    } else {
-                        exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
-                        exchange.getResponseSender().send("Unknown request");
-                    }
-                })
+                .setHandler(DevToolbench::handleRequest)
                 .build();
         server.start();
     }
 
-    static {
-        register("/listFiles", new ListFilesOperation());
-        register("/readFile", new ReadFileOperation());
-        register("/writeFile", new WriteFileOperation());
-        // TODO: Register other handlers here
+    private static void handleRequest(HttpServerExchange exchange) {
+        try {
+            String path = exchange.getRequestPath();
+            if (STATICFILES.containsKey(path)) {
+                handleStaticFile(exchange, path);
+            } else {
+                HttpHandler handler = HANDLERS.get(path);
+                if (handler != null) {
+                    handler.handleRequest(exchange);
+                } else {
+                    exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
+                    exchange.getResponseSender().send("Unknown request");
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    static void register(String path, HttpHandler handler) {
-        handlers.put(path, handler);
+    private static void handleStaticFile(HttpServerExchange exchange, String path) {
+        String content = STATICFILES.get(path).get();
+        exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
+        exchange.getResponseSender().send(content);
     }
 }
