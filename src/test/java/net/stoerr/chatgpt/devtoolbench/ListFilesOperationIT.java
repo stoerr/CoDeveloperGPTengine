c5914f9e7028e097377c394d0bb573ce09ccf6ff
Hans-Peter Stoerr <hans-peter+git-noreply@stoerr.net>
2023-06-20 17:07:39 +0000

    ChatGPT: list files for yourself, see pom.xml for present libraries and view DevToolbenchIT .          We want to separate the tests in DevToolbenchIT into several tests, one per each operation. Also, the tests should be using a HttpClient 4.          In the folder where DevToolbenchIT is create a class AbstractIT that is the abstract base class for all such tests. Please create AbstractIT that has methods common to many such tests, and a test ListFilesOperationIT that should have the test methods specific to the ListFilesOperation (/listFiles).          Think aloud step by step, read those files first and try to extract common code into methods. First describe what should go into Abstract IT and then write AbstractIT and ListFilesOperationIT.

---
+++ src/test/java/net/stoerr/chatgpt/devtoolbench/ListFilesOperationIT.java
@@ -0,0 +1,16 @@
+package net.stoerr.chatgpt.devtoolbench;
+
+import org.junit.Test;
+
+public class ListFilesOperationIT extends AbstractIT {
+
+    @Test
+    public void testListFilesOperation() throws Exception {
+        checkResponse("/listFiles?path=.", "GET", null, 200, "listFiles.txt");
+        checkResponse("/listFiles?path=subdir", "GET", null, 200, "listFilesSubdir.txt");
+        checkResponse("/listFiles?path=.&filenameRegex=.*%5C.txt", "GET", null, 200, "listFilesFilenameRegex.txt");
+        checkResponse("/listFiles?path=.&grepRegex=testcontent", "GET", null, 200, "listFilesGrepRegex.txt");
+        checkResponse("/listFiles?path=.&filenameRegex=.*%5C.txt&grepRegex=testcontent",
+                "GET", null, 200, "listFilesBothRegex.txt");
+    }
+}
