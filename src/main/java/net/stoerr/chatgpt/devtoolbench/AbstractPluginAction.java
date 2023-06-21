1abb27e9ee76eea52641e0c3de5d319dd045b42e
Hans-Peter Stoerr <hp.stoerr@ist-software.com>
2023-06-21 23:40:24 +0200

    sort files to make file ordering predictable

--- src/main/java/net/stoerr/chatgpt/devtoolbench/AbstractPluginAction.java
+++ src/main/java/net/stoerr/chatgpt/devtoolbench/AbstractPluginAction.java
@@ -73,7 +73,7 @@ public abstract class AbstractPluginAction implements HttpHandler {
                             return false;
                         }
                     }
-                });
+                }).sorted();
     }
 
     /**
