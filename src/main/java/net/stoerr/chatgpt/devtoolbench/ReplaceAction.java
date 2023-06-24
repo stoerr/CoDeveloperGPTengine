7da11f8b24c1a0756ad9842a4a81d4380f0d5a3d
Hans-Peter Stoerr <hp.stoerr@ist-software.com>
2023-06-24 09:22:55 +0200

    ReplaceActionIT incl. fixes

--- src/main/java/net/stoerr/chatgpt/devtoolbench/ReplaceAction.java
+++ src/main/java/net/stoerr/chatgpt/devtoolbench/ReplaceAction.java
@@ -2,7 +2,6 @@ package net.stoerr.chatgpt.devtoolbench;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
@@ -10,7 +9,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import io.undertow.server.HttpServerExchange;
-import io.undertow.util.Headers;
 
 public class ReplaceAction extends AbstractPluginAction {
 
@@ -21,45 +19,47 @@ public class ReplaceAction extends AbstractPluginAction {
 
     @Override
     public String openApiDescription() {
-        return "/replace:\n" +
-                "    post:\n" +
-                "      operationId: replace\n" +
-                "      summary: Replace occurrences of a string in a file.\n" +
-                "      parameters:\n" +
-                "        - name: path\n" +
-                "          in: query\n" +
-                "          description: Path of the file\n" +
-                "          required: true\n" +
-                "          schema:\n" +
-                "            type: string\n" +
-                "        - name: searchString\n" +
-                "          in: query\n" +
-                "          description: String to be replaced\n" +
-                "          required: true\n" +
-                "          schema:\n" +
-                "            type: string\n" +
-                "        - name: replacement\n" +
-                "          in: query\n" +
-                "          description: Replacement string\n" +
-                "          required: true\n" +
-                "          schema:\n" +
-                "            type: string\n" +
-                "      requestBody:\n" +
-                "        required: false\n" +
-                "        content:\n" +
-                "          application/json:\n" +
-                "            schema:\n" +
-                "              type: object\n" +
-                "              properties:\n" +
-                "                content:\n" +
-                "                  type: string\n" +
-                "      responses:\n" +
-                "        ''204'':\n" +
-                "          description: File updated successfully\n" +
-                "        '404':\n" +
-                "          description: Invalid parameter\n" +
-                "        '500':\n" +
-                "          description: Server error";
+        return """
+                  /replace:
+                    post:
+                      operationId: replace
+                      summary: Replace occurrences of a string in a file.
+                      parameters:
+                        - name: path
+                          in: query
+                          description: relative path to file
+                          required: true
+                          schema:
+                            type: string
+                        - name: searchString
+                          in: query
+                          description: String to be replaced
+                          required: true
+                          schema:
+                            type: string
+                        - name: replacement
+                          in: query
+                          description: Replacement string
+                          required: true
+                          schema:
+                            type: string
+                      requestBody:
+                        required: true
+                        content:
+                          application/json:
+                            schema:
+                              type: object
+                              properties:
+                                content:
+                                  type: string
+                      responses:
+                        '204':
+                          description: File updated successfully
+                        '400':
+                          description: Invalid parameter
+                        '500':
+                          description: Server error
+                """.stripIndent();
     }
 
     @Override
@@ -68,7 +68,6 @@ public class ReplaceAction extends AbstractPluginAction {
     }
 
     private void handleBody(HttpServerExchange exchange, String json) {
-        exchange.startBlocking();
         String path = exchange.getQueryParameters().get("path").getFirst();
         String searchString = exchange.getQueryParameters().get("searchString").getFirst();
         String replacement = exchange.getQueryParameters().get("replacement").getFirst();
@@ -81,16 +80,14 @@ public class ReplaceAction extends AbstractPluginAction {
 
         Path filePath = DevToolbench.currentDir.resolve(path);
         try {
-            String content = new String(Files.readAllBytes(filePath), UTF_8);
+            String content = Files.readString(filePath, UTF_8);
             content = content.replace(searchString, replacement);
-            Files.write(filePath, content.getBytes(UTF_8));
-
-            exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain; charset=UTF-8");
-            exchange.getResponseSender().send("File updated successfully");
+            Files.writeString(filePath, content, UTF_8);
+            exchange.setStatusCode(204);
         } catch (NoSuchFileException e) {
-            sendError(exchange, 404, "File not found");
+            throw sendError(exchange, 404, "File not found: " + filePath);
         } catch (IOException e) {
-            sendError(exchange, 500, "Error reading or writing file : " + e.toString());
+            throw sendError(exchange, 500, "Error reading or writing file : " + e);
         }
     }
 
