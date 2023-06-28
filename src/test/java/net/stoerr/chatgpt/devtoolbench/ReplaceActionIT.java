d25e8a5031a2a515c9537ec3815f45ca43cbae3b
Hans-Peter Stoerr <hp.stoerr@ist-software.com>
2023-06-28 17:55:13 +0000

    rework ReplaceActionIT to reduce filecount

--- src/test/java/net/stoerr/chatgpt/devtoolbench/ReplaceActionIT.java
+++ src/test/java/net/stoerr/chatgpt/devtoolbench/ReplaceActionIT.java
@@ -1,6 +1,7 @@
 package net.stoerr.chatgpt.devtoolbench;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.hamcrest.CoreMatchers.is;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -14,9 +15,10 @@ public class ReplaceActionIT extends AbstractActionIT {
         try {
             String content = Files.readString(Paths.get("src/test/resources/testdir/secondfile.md"), UTF_8);
             Files.writeString(Paths.get("src/test/resources/testdir/replace.txt"), content, UTF_8);
-            checkResponse("/replaceInFile?path=replace.txt", "POST",
+            String response = checkResponse("/replaceInFile?path=replace.txt", "POST",
                     "{\"pattern\":\"duck\",\"literalReplacement\":\"goose\",\"multiple\":true}"
-                    , 200, "replace-successfulmulti.txt");
+                    , 200, null);
+            collector.checkThat(response, is("Replaced 12 occurrences of pattern; modified lines  1 - 3,  11 - 13, 16"));
             checkResponse("/readFile?path=replace.txt", "GET", null, 200, "replace-successfulmulti-replaced.txt");
         } finally {
             Files.deleteIfExists(Paths.get("src/test/resources/testdir/replace.txt"));
@@ -25,16 +27,18 @@ public class ReplaceActionIT extends AbstractActionIT {
 
     @Test
     public void testComplainAboutMultiplesSinceNoMatch() throws Exception {
-        checkResponse("/replaceInFile?path=secondfile.md", "POST",
+        String response = checkResponse("/replaceInFile?path=secondfile.md", "POST",
                 "{\"pattern\":\"neverthereinthefile\",\"literalReplacement\":\"goose\"}"
-                , 400, "replace-multiplenotrequested1.txt");
+                , 400, null);
+        collector.checkThat(response, is("Found no occurrences of pattern."));
     }
 
     @Test
     public void testComplainAboutMultiplesSinceManyMatches() throws Exception {
-        checkResponse("/replaceInFile?path=secondfile.md", "POST",
+        String response = checkResponse("/replaceInFile?path=secondfile.md", "POST",
                 "{\"pattern\":\"duck\",\"literalReplacement\":\"goose\"}"
-                , 400, "replace-multiplenotrequested2.txt");
+                , 400, null);
+        collector.checkThat(response, is("Found 12 occurrences, but expected exactly one because parameter multiple = false."));
     }
 
     @Test
@@ -46,36 +50,40 @@ public class ReplaceActionIT extends AbstractActionIT {
 
     @Test
     public void testBothReplacementsGiven() throws Exception {
-        checkResponse("/replaceInFile?path=secondfile.md", "POST",
+        String response = checkResponse("/replaceInFile?path=secondfile.md", "POST",
                 "{\"pattern\":\"duck\",\"literalReplacement\":\"goose\",\"replacementWithGroupReferences\":\"goose\",\"multiple\":true}"
-                , 400, "replace-both-replacements-given.txt");
+                , 400, null);
+        collector.checkThat(response, is("Either literalReplacement or replacementWithGroupReferences must be given, but not both."));
     }
 
     @Test
     public void testNoReplacementsGiven() throws Exception {
-        checkResponse("/replaceInFile?path=secondfile.md", "POST",
+        String response = checkResponse("/replaceInFile?path=secondfile.md", "POST",
                 "{\"pattern\":\"duck\",\"multiple\":true}"
-                , 400, "replace-no-replacements-given.txt");
+                , 400, null);
+        collector.checkThat(response, is("Either literalReplacement or replacementWithGroupReferences must be given."));
     }
 
     @Test
     public void testReplacementWithGroupReferencesNoGroup() throws Exception {
-        checkResponse("/replaceInFile?path=secondfile.md", "POST",
+        String response = checkResponse("/replaceInFile?path=secondfile.md", "POST",
                 "{\"pattern\":\"duck\",\"replacementWithGroupReferences\":\"goose\",\"multiple\":true}"
-                , 400, "replace-replacement-with-group-references-no-group.txt");
+                , 400, null);
+        collector.checkThat(response, is("don't use replacementWithGroupReferences if there are no group references."));
     }
 
     @Test
     public void testReplacementWithGroupReferencesSuccessful() throws Exception {
         try {
             String content = Files.readString(Paths.get("src/test/resources/testdir/secondfile.md"), UTF_8);
-            Files.writeString(Paths.get("src/test/resources/testdir/replace.txt"), content, UTF_8);
-            checkResponse("/replaceInFile?path=replace.txt", "POST",
-                    "{\"pattern\":\"duck\",\"replacementWithGroupReferences\":\"goose\",\"multiple\":true}"
-                    , 200, "replace-successfulmulti.txt");
-            checkResponse("/readFile?path=replace.txt", "GET", null, 200, "replace-successfulmulti-replaced.txt");
+            Files.writeString(Paths.get("src/test/resources/testdir/replace2.txt"), content, UTF_8);
+            String response = checkResponse("/replaceInFile?path=replace2.txt", "POST",
+                    "{\"pattern\":\"(duck)\",\"replacementWithGroupReferences\":\"goose$1\",\"multiple\":true}"
+                    , 200, null);
+            collector.checkThat(response, is("Replaced 12 occurrences of pattern; modified lines  1 - 3,  11 - 13, 16"));
+            checkResponse("/readFile?path=replace2.txt", "GET", null, 200, "replace-successfulmulti-replaced2.txt");
         } finally {
-            Files.deleteIfExists(Paths.get("src/test/resources/testdir/replace.txt"));
+            Files.deleteIfExists(Paths.get("src/test/resources/testdir/replace2.txt"));
         }
     }
 
