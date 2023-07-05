21e3deb2722be96832b4094f81358bdbea3447a6
Hans-Peter Stoerr <hans-peter+git-noreply@stoerr.net>
2023-07-05 16:50:49 +0000

    continue

--- src/main/java/net/stoerr/chatgpt/devtoolbench/DevToolBench.java
+++ src/main/java/net/stoerr/chatgpt/devtoolbench/DevToolBench.java
@@ -1,14 +1,12 @@
 package net.stoerr.chatgpt.devtoolbench;
 
-import static net.stoerr.chatgpt.devtoolbench.AbstractPluginAction.sendError;
-
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Comparator;
+import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -23,22 +21,43 @@ import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.servlet.FilterHolder;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.util.resource.Resource;
 
-import io.undertow.server.HttpHandler;
-import io.undertow.server.HttpServerExchange;
-import io.undertow.util.Headers;
-import io.undertow.util.HttpString;
-import io.undertow.util.Methods;
+import jakarta.servlet.DispatcherType;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class DevToolBench {
 
+    public static final Filter CORSFILTER = new Filter() {
+        @Override
+        public void doFilter(ServletRequest rawRequest, ServletResponse rawResponse, FilterChain chain) throws IOException, ServletException {
+            // if it's an OPTIONS request, we need to give a CORS response like method giveCORSResponse below
+            if (rawRequest instanceof HttpServletRequest request && rawResponse instanceof HttpServletResponse response) {
+                if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+                    response.addHeader("Access-Control-Allow-Origin", "https://chat.openai.com");
+                    response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+                    if (request.getHeader("Access-Control-Request-Headers") != null) {
+                        response.addHeader("Access-Control-Allow-Headers", request.getHeader("Access-Control-Request-Headers"));
+                    }
+                    response.addHeader("Access-Control-Max-Age", "3600");
+                    response.addHeader("Allow", "*");
+                    response.setStatus(200);
+                    return;
+                }
+            }
+            chain.doFilter(rawRequest, rawResponse);
+        }
+    };
     static Path currentDir = Paths.get(".").normalize().toAbsolutePath();
 
     private static final Map<String, Supplier<String>> STATICFILES = new HashMap<>();
@@ -109,6 +128,22 @@ public class DevToolBench {
                 resp.getWriter().write(OPENAPI_DESCR_START.replace("THEPORT", String.valueOf(port)) + pathDescriptions);
             }
         }), "/devtoolbench.yaml");
+
+        context.addFilter(new FilterHolder(CORSFILTER), "/*", EnumSet.of(DispatcherType.REQUEST));
+
+        context.addFilter(new FilterHolder((ServletRequest request, ServletResponse response, FilterChain chain) -> {
+            HttpServletRequest req = (HttpServletRequest) request;
+            TbUtils.logInfo(req.getMethod() + " " + req.getRequestURI() + (req.getQueryString() != null && !req.getQueryString().isEmpty() ? "?" + req.getQueryString() : ""));
+            try {
+                chain.doFilter(request, response);
+            } catch (ExecutionAbortedException e) {
+                TbUtils.logInfo("Aborted and problem reported to ChatGPT : " + e.getMessage());
+            } catch (Exception e) {
+                TbUtils.logError("Bug! Abort handling request " + ((HttpServletRequest) request).getRequestURI());
+                TbUtils.logStacktrace(e);
+                throw e;
+            }
+        }), "/*", EnumSet.of(DispatcherType.REQUEST));
     }
 
     public static void main(String[] args) throws Exception {
@@ -162,71 +197,6 @@ public class DevToolBench {
 
     public static void stop() throws Exception {
         server.stop();
-    }
-
-    private static void handleRequest(HttpServerExchange exchange) {
-        try {
-            TbUtils.logInfo(exchange.getRequestMethod() + " " + exchange.getRequestURI() +
-                    (exchange.getQueryString() != null && !exchange.getQueryString().isEmpty() ? "?" + exchange.getQueryString() : ""));
-            TbUtils.logRequest(exchange);
-            String path = exchange.getRequestPath();
-            exchange.getResponseHeaders().put(HttpString.tryFromString("Access-Control-Allow-Origin"), "https://chat.openai.com");
-            if (exchange.getRequestMethod().equals(Methods.OPTIONS)) {
-                giveCORSResponse(exchange);
-            } else if (STATICFILES.containsKey(path)) {
-                handleStaticFile(exchange, path);
-            } else if (path.equals("/icon.png")) {
-                byte[] bytes;
-                try (InputStream resourceAsStream = DevToolBench.class.getResourceAsStream("/icon.png")) {
-                    bytes = resourceAsStream.readAllBytes();
-                }
-                exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "image/png");
-                exchange.setStatusCode(200);
-                exchange.setResponseContentLength(bytes.length);
-                exchange.getResponseSender().send(ByteBuffer.wrap(bytes));
-            } else {
-                HttpHandler handler = HANDLERS.get(path);
-                if (handler != null) {
-                    handler.handleRequest(exchange);
-                    TbUtils.logInfo("Response: " + exchange.getStatusCode() + " " + exchange.getResponseHeaders());
-                } else {
-                    throw sendError(exchange, 404, "Unknown request");
-                }
-            }
-        } catch (ExecutionAbortedException e) {
-            TbUtils.logInfo("Aborted and problem reported to ChatGPT : " + e.getMessage());
-        } catch (Exception e) {
-            TbUtils.logError("Bug! Abort handling request " + exchange.getRequestURL());
-            TbUtils.logStacktrace(e);
-        } finally {
-            exchange.endExchange();
-        }
-    }
-
-    private static void giveCORSResponse(HttpServerExchange exchange) {
-        // already there: exchange.getResponseHeaders().add(HttpString.tryFromString("Access-Control-Allow-Origin"), "*");
-        exchange.getResponseHeaders().put(HttpString.tryFromString("Access-Control-Allow-Methods"), "GET, POST, PUT, DELETE");
-        if (exchange.getRequestHeaders().contains("Access-control-request-headers")) {
-            exchange.getResponseHeaders().add(HttpString.tryFromString("Access-Control-Allow-Headers"), exchange.getRequestHeaders().getFirst("access-control-request-headers"));
-        }
-        exchange.getResponseHeaders().put(HttpString.tryFromString("Access-Control-Max-Age"), "3600");
-        exchange.getResponseHeaders().put(HttpString.tryFromString("Allow"), "*");
-        exchange.setStatusCode(200);
-    }
-
-    private static void handleStaticFile(HttpServerExchange exchange, String path) {
-        String content = STATICFILES.get(path).get();
-        if (content != null && !content.isBlank()) {
-            exchange.setStatusCode(200);
-            if (content.contains("<html>")) {
-                exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/html; charset=UTF-8");
-            } else {
-                exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain; charset=UTF-8");
-            }
-            exchange.getResponseSender().send(content);
-        } else {
-            throw sendError(exchange, 404, "File not found");
-        }
     }
 
 }
