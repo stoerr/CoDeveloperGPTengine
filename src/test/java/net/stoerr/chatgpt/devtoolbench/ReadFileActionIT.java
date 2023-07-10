07a7df8789c56c848a1d9b6557b5f2585a1bd1ce
Hans-Peter Stoerr <hans-peter+git-noreply@stoerr.net>
2023-07-10 16:10:26 +0000

    cont

--- src/test/java/net/stoerr/chatgpt/devtoolbench/ReadFileActionIT.java
+++ src/test/java/net/stoerr/chatgpt/devtoolbench/ReadFileActionIT.java
@@ -1,28 +1,37 @@
 package net.stoerr.chatgpt.devtoolbench;
 
-import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertEquals;
 
-public class ReadFileActionIT {
+import org.junit.Test;
+
+public class ReadFileActionIT extends AbstractActionIT {
 
     @Test
-    public void testReadWithoutParameters() {
-        // TODO: Implement this test
+    public void testReadWithoutParameters() throws Exception {
+        String response = checkResponse("/readFile?path=firstfile.txt", "GET", null, 200, null);
+        assertEquals("Hello!\nJust a test.\n", response);
     }
 
     @Test
-    public void testReadWithMaxLines() {
-        // TODO: Implement this test
+    public void testReadWithMaxLines() throws Exception {
+        String response = checkResponse("/readFile?path=firstfile.txt&maxLines=1", "GET", null, 200, null);
+        String[] lines = response.split("\n");
+        assertEquals(1, lines.length);
     }
 
     @Test
-    public void testReadWithStartLine() {
-        // TODO: Implement this test
+    public void testReadWithStartLine() throws Exception {
+        String response = checkResponse("/readFile?path=firstfile.txt&startLine=2", "GET", null, 200, null);
+        String firstLine = response.split("\n")[0];
+        assertEquals("Just a test.", firstLine);
     }
 
     @Test
-    public void testReadWithMaxLinesAndStartLine() {
-        // TODO: Implement this test
+    public void testReadWithMaxLinesAndStartLine() throws Exception {
+        String response = checkResponse("/readFile?path=firstfile.txt&maxLines=1&startLine=2", "GET", null, 200, null);
+        String[] lines = response.split("\n");
+        assertEquals(1, lines.length);
+        assertEquals("Just a test.", lines[0]);
     }
 }
