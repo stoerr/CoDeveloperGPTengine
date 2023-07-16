8ac2415f597004cf01a826d98cad7b818838a8e5
Hans-Peter Stoerr <hp.stoerr@ist-software.com>
2023-07-16 10:18:18 +0200

    reduce logging during build

--- .cgptdevbench/build.sh
+++ .cgptdevbench/build.sh
@@ -1,3 +1,10 @@
 # Plugin Action: maven build incl. running unit- and integrationtests
 echo executing mvn clean install
-mvn -B clean install 2>&1
+mvn -B clean install > build.log 2>&1
+if [ $? -ne 0 ]; then
+  echo "mvn clean install failed, see build.log"
+  exit 1
+else
+  echo "mvn clean install successful"
+  exit 0
+fi
