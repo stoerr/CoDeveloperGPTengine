99c45cbd4ab98d2650d1f228ca407f43fb4bc91d
Hans-Peter Stoerr <hans-peter+git-noreply@stoerr.net>
2023-08-12 16:50:17 +0200

    get rid of uneccessary documentation of UrlAction

--- src/test/java/net/stoerr/chatgpt/devtoolbench/UrlActionIT.java
+++ src/test/java/net/stoerr/chatgpt/devtoolbench/UrlActionIT.java
@@ -9,7 +9,7 @@ public class UrlActionIT extends AbstractActionIT {
     public void testValidUrl() throws Exception {
         TbUtils.logInfo("\nUrlActionIT.testValidUrl");
         String validUrl = "https://www.example.com";
-        String response = checkResponse("/fetchUrlContent?url=" + validUrl, "GET", null, 200, null);
+        String response = checkResponse("/fetchUrlTextContent?url=" + validUrl, "GET", null, 200, null);
         collector.checkThat(response, CoreMatchers.containsString("Example Domain"));
         System.out.printf("Response: %s\n", response);
     }
@@ -18,7 +18,7 @@ public class UrlActionIT extends AbstractActionIT {
     public void testInvalidUrl() throws Exception {
         TbUtils.logInfo("\nUrlActionIT.testInvalidUrl");
         String invalidUrl = "https://invalid.url.that.does.not.exist";
-        String response = checkResponse("/fetchUrlContent?url=" + invalidUrl, "GET", null, 404, null);
+        String response = checkResponse("/fetchUrlTextContent?url=" + invalidUrl, "GET", null, 400, null);
         System.out.printf("Response: %s\n", response);
     }
 }
