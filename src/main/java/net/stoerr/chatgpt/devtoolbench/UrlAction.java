cb442b00286f447ca427eda57687da57dbd3c23a
Hans-Peter Stoerr <hp.stoerr@ist-software.com>
2023-08-22 18:41:19 +0000

    add optional markdown conversion to fetchUrlTextContent

--- src/main/java/net/stoerr/chatgpt/devtoolbench/UrlAction.java
+++ src/main/java/net/stoerr/chatgpt/devtoolbench/UrlAction.java
@@ -1,16 +1,23 @@
 package net.stoerr.chatgpt.devtoolbench;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
 
 import javax.net.ssl.SSLHandshakeException;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Node;
+import org.apache.commons.lang3.tuple.Pair;
 
+import io.github.furstenheim.CodeBlockStyle;
+import io.github.furstenheim.CopyDown;
+import io.github.furstenheim.HeadingStyle;
+import io.github.furstenheim.Options;
+import io.github.furstenheim.OptionsBuilder;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -21,6 +28,7 @@ public class UrlAction extends AbstractPluginAction {
         return "/fetchUrlTextContent";
     }
 
+    // also add an optional parameter raw that returns the raw content of the URL without converting to markdown
     @Override
     public String openApiDescription() {
         return """
@@ -34,6 +42,12 @@ public class UrlAction extends AbstractPluginAction {
                           required: true
                           schema:
                             type: string
+                        - name: raw
+                          description: return raw html content without converting to markdown
+                          in: query
+                          required: false
+                          schema:
+                            type: boolean
                       responses:
                         '200':
                           content:
@@ -51,35 +65,40 @@ public class UrlAction extends AbstractPluginAction {
             resp.getWriter().write("URL parameter is missing or empty.");
             return;
         }
+        boolean raw = Boolean.TRUE.toString().equalsIgnoreCase(req.getParameter("raw"));
 
         try {
-            Document document;
+            Pair<String, String> contentinfo;
             try {
-                document = Jsoup.parse(new URL(urlString), 10000);
+                contentinfo = fetchContentFromUrl(urlString);
             } catch (MalformedURLException e) {
                 if (!urlString.startsWith("https://") && !urlString.startsWith("http://")) {
                     try {
-                        document = Jsoup.parse(new URL("https://" + urlString), 10000);
+                        contentinfo = fetchContentFromUrl("https://" + urlString);
                     } catch (SSLHandshakeException e1) {
-                        document = Jsoup.parse(new URL("http://" + urlString), 10000);
+                        contentinfo = fetchContentFromUrl("http://" + urlString);
                     }
                 } else {
                     throw e;
                 }
             }
 
-            // iterate through all tags in the document and collect text contents separated with \n
-            // take care to only use the text of the current element and not of its children
-            // but <p> hallo <b> you </b> here </p> should be "hallo\nyou\nhere"
-            StringBuilder content = new StringBuilder();
-            // append document title to content
-            if (document.title() != null && !document.title().isEmpty()) {
-                content.append(document.title()).append("\n\n");
+            String contentString = contentinfo.getRight();
+            String contentType = contentinfo.getLeft();
+            // remove trailing ;charset=... from content type
+            int charsetIndex = contentType.indexOf(";");
+            if (charsetIndex > 0) {
+                contentType = contentType.substring(0, charsetIndex);
             }
-            // iterate recursively over childNodes() and collect text of TextNodes into content
-            appendChildText(document.body(), content);
 
-            byte[] bytes = content.toString().getBytes(StandardCharsets.UTF_8);
+            if (contentType.startsWith("text/html") && !raw) {
+                contentString = htmlToMarkdown(contentString);
+                contentString = "markdown for " + contentType + " content of " + urlString + "\n\n" + contentString;
+            } else {
+                contentString = contentType + " content of URL " + urlString + "\n\n" + contentString;
+            }
+
+            byte[] bytes = contentString.getBytes(StandardCharsets.UTF_8);
             resp.setContentLength(bytes.length);
             resp.setContentType("text/plain;charset=UTF-8");
             resp.setStatus(HttpServletResponse.SC_OK);
@@ -90,13 +109,26 @@ public class UrlAction extends AbstractPluginAction {
         }
     }
 
-    private void appendChildText(Node node, StringBuilder content) {
-        for (Node child : node.childNodes()) {
-            if (child instanceof org.jsoup.nodes.TextNode) {
-                content.append(child.toString()).append("\n");
-            } else {
-                appendChildText(child, content);
-            }
+    private static String htmlToMarkdown(String contentString) {
+        OptionsBuilder optionsBuilder = OptionsBuilder.anOptions();
+        Options options = optionsBuilder.withHeadingStyle(HeadingStyle.ATX)
+                .withCodeBlockStyle(CodeBlockStyle.FENCED)
+                .withBulletListMaker("-")
+                .withEmDelimiter("*")
+                .withStrongDelimiter("**")
+                .withHr("---")
+                .build();
+        CopyDown converter = new CopyDown(options);
+        contentString = converter.convert(contentString);
+        return contentString;
+    }
+
+    private Pair<String, String> fetchContentFromUrl(String urlString) throws IOException {
+        URL url = new URL(urlString);
+        URLConnection connection = url.openConnection();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+            String contentType = connection.getContentType();
+            return Pair.of(contentType, reader.lines().collect(Collectors.joining("\n")));
         }
     }
 
