19c7819f6d0892957158b89a229407b2f2bdb7ee
Hans-Peter Stoerr <hans-peter+git-noreply@stoerr.net>
2023-10-28 21:01:10 +0200

    limit number of tokens in ReadFileAction

--- src/main/java/net/stoerr/chatgpt/devtoolbench/ReadFileAction.java
+++ src/main/java/net/stoerr/chatgpt/devtoolbench/ReadFileAction.java
@@ -7,11 +7,28 @@ import java.nio.file.Path;
 import java.util.List;
 import java.util.stream.Stream;
 
+import com.knuddels.jtokkit.Encodings;
+import com.knuddels.jtokkit.api.Encoding;
+import com.knuddels.jtokkit.api.EncodingRegistry;
+import com.knuddels.jtokkit.api.EncodingType;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 // curl -is http://localhost:3001/readFile?path=somefile.txt
 public class ReadFileAction extends AbstractPluginAction {
+
+    /**
+     * The maximum number of ChatGPT tokes we output if there is no maxLines parameter given
+     */
+    private static final int MAXTOKENS_NOLIMIT = 2000;
+
+    protected final EncodingRegistry registry = Encodings.newDefaultEncodingRegistry();
+
+    /**
+     * Tokenizer used for GPT-3.5 and GPT-4.
+     */
+    protected final Encoding enc = registry.getEncoding(EncodingType.CL100K_BASE);
 
     @Override
     public String getUrl() {
@@ -66,6 +83,7 @@ public class ReadFileAction extends AbstractPluginAction {
                 throw sendError(resp, HttpServletResponse.SC_BAD_REQUEST, "Path is a directory - please use listFiles for listing directory contents: " + path);
             }
             List<String> lines;
+            int fulllinecount = (int) Files.lines(path).count();
             try (Stream<String> linesStream = Files.lines(path)) {
                 lines = linesStream
                         .skip(startLine - 1L)
@@ -73,8 +91,22 @@ public class ReadFileAction extends AbstractPluginAction {
                         .toList();
             }
             String content = String.join("\n", lines) + "\n";
+            if (maxLines == Integer.MAX_VALUE) {
+                int numtokens = enc.encode(content).size();
+                int dropped = 0;
+                if (numtokens > MAXTOKENS_NOLIMIT) {
+                    do {
+                        // drop the 10% last lines of lines and try again
+                        int numlinesToDrop = (int) (lines.size() * 0.1) + 1;
+                        dropped += numlinesToDrop;
+                        lines = lines.subList(0, lines.size() - numlinesToDrop);
+                        content = String.join("\n", lines) + "\n";
+                    } while (enc.encode(content).size() > MAXTOKENS_NOLIMIT);
+                    log("Dropped " + dropped + " lines to reduce token count from " + numtokens + " to " + enc.encode(content).size() + " in " + path);
+                }
+            }
             if (maxLines != Integer.MAX_VALUE || startLine != 1) {
-                content = "File " + DevToolBench.currentDir.relativize(path) + " lines " + startLine + " to line " + (startLine + lines.size() - 1) + "\n\n" + content;
+                content = "File " + DevToolBench.currentDir.relativize(path) + " lines " + startLine + " to line " + (startLine + lines.size() - 1) + " of " + fulllinecount + " lines\n\n" + content;
             }
             byte[] bytes = content.getBytes(StandardCharsets.UTF_8);
             resp.setContentLength(bytes.length);
