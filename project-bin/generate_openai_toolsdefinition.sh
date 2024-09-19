a66466b137cd95b5b8bd58a7a4f69353faa86217
Dr. Hans-Peter Stoerr <hans-peter+git-noreply@stoerr.net>
2024-09-19 22:42:15 +0200

    move tools definition file for chatgpt script to the server

--- project-bin/generate_openai_toolsdefinition.sh
+++ project-bin/generate_openai_toolsdefinition.sh
@@ -5,5 +5,5 @@ cd $(dirname $0)/../
 aigenpipeline -wvf -o src/main/resources/static/codeveloperengine-toolsdefinition.json \
     -p $scriptdir/generate_openai_toolsdefinition.prompt src/test/resources/test-expected/codeveloperengine.yaml
 
-aigenpipeline -wvf -o chatgptscriptrun/codeveloperengine-chatgptscript-toolsdefinition.json \
+aigenpipeline -wvf -o src/main/resources/static/codeveloperengine-chatgptscript-toolsdefinition.json \
     -p $scriptdir/generate_chatgpt_script_toolsdefinition.prompt src/main/resources/static/codeveloperengine-toolsdefinition.json
